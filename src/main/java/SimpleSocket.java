35bc8168a45efeb9c7512490c6c82c0427c2fcea
alex <alex-vav@list.ru>
2022-07-11 00:02:32 +0300

    add comment in ReadMy.txt

--- src/main/java/SimpleSocket.java
+++ src/main/java/SimpleSocket.java
@@ -5,19 +5,21 @@ import java.net.Socket;
 
 public class SimpleSocket {
     public static void main(String[] args) throws IOException {
+//        Соединение с сервером java-course.ru по 80 порту
         Socket socket = new Socket("java-course.ru", 80);
 
         InputStream is = socket.getInputStream();
         OutputStream os = socket.getOutputStream();
 
-//        String command = "GET / HTTP/1.1\r\nHost:java-course.ru\r\n\r\n";
-//        String command = "GET /examples/ HTTP/1.1\r\nHost:java-course.ru\r\n\r\n";
-//        String command = "GET /docs/ HTTP/1.1\r\nHost:java-course.ru\r\n\r\n";
+//Формитуем HTTPзапрос,
         String command = "GET /sitemap.xml HTTP/1.1\r\nHost:java-course.ru\r\n\r\n";
+//        Формируем  запрос
         os.write(command.getBytes());
+//        Немеденно отправляем запрос
         os.flush();
 
         int c = 0;
+//        Читаем ответ
         while ((c = is.read()) != -1){
             System.out.print((char) c);
         }
